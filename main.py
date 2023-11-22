9bf7b496d380433348a4fc4d365e624680a4c1b4
magicFeirl <2100709458@qq.com>
2023-11-22 19:05:21 +0800

    feat: 优化消息格式

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import re
 from datetime import datetime
+from string import Template
 
 import requests
 import pytz
@@ -50,17 +52,29 @@ def format_live_message(room_id: str):
     title = room_info.get('title')
     user_cover = room_info.get('user_cover')
     live_status = room_info.get('live_status')
+    live_time = room_info.get('live_time')
+    if live_time[:4] != '0000':
+        minutes = (datetime.now() - datetime.strptime(live_time, '%Y-%m-%d %H:%M:%S')).total_seconds() / 60
+    else:
+        minutes = 0
 
+    minutes = round(minutes)
+    
     master_info_resp = get_room_master_info(uid)
     user_info = master_info_resp['data']['info']
     username = user_info['uname']
 
-    now = datetime.now(tz=pytz.timezone('Asia/Shanghai'))
-    message = f'{title}\n直播状态:{LIVE_STATUS_DICT[live_status]}\n时间(UTC+8) {now}'.encode(
-        'utf-8')
+    message = Template('''
+        $title 
+        开播时间 $live_time
 
-    return live_status, {
-        'message': message,
+        $minutes 分钟前
+    ''').substitute(title=title, live_time=live_time, minutes=minutes)
+
+    message = re.sub(r'\n\s+(.)', r'\n\g<1>', message.strip())
+
+    return live_status, username, {
+        'message': message.encode(),
         'headers': {
             'Title': f'{username}开播了'.encode(),
             'Attach': user_cover,
@@ -70,22 +84,24 @@ def format_live_message(room_id: str):
 
 
 def main():
-
     jdata = db.load_db()
 
     for room_id in config.LIVE_ROOM_ID_LIST:
-        live_status, content = format_live_message(room_id=room_id)
+        live_status, username, content = format_live_message(room_id=room_id)
         pre_live_status = jdata[room_id]['live_status']
         
         if live_status == 1 and pre_live_status != 1:
             ntfy.send('ac8888', **content)
-            print(content)
+            title, message = content['headers']['Title'], content['message']
+            print(title.decode('utf-8'), '\n', message.decode('utf-8'))
+
 
         jdata[room_id]['live_status'] = live_status
         jdata[room_id]['last_check'] = str(datetime.now())
+        jdata[room_id]['username'] = username
 
     # python dict 的 key -> value 没变不会触发 __setitem__，导致数据没有更新，需要用时间戳强制触发一次
-    jdata['update_timestamp'] = datetime.now().timestamp()
+    jdata['update_time'] = str(datetime.now())
 
 
 main()
